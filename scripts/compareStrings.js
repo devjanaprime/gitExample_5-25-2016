53a4c0185c88f9c4015a10d84ceb7085b4cdcbfb
mculotta120 <margaret.culotta@gmail.com>
2016-05-25 16:30:14 -0500

    check for empty input

--- scripts/compareStrings.js
+++ scripts/compareStrings.js
@@ -2,9 +2,11 @@ var longerString  = function(){
   // get user input
   var string1 = document.getElementById('word1').value;
   var string2 = document.getElementById('word2').value;
-
+  if (string1.length === 0 || string2.length === 0){
+  console.log("Please enter two words to compare.");
+}
   // check for whitespace
-  if( string1.indexOf( " " ) >= 0 || string2.indexOf( " " ) >= 0 ){
+  else if( string1.indexOf( " " ) >= 0 || string2.indexOf( " " ) >= 0 ){
     console.log("Whitspace ROCKS!!!");
   }
   else {
@@ -15,4 +17,4 @@ var longerString  = function(){
       console.log( string2 + " is the longest string" );
     }
   }
-}
+};

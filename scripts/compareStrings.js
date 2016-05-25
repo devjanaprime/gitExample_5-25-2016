6f1294184305d9148f15ef42604d23073c8825c0
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-05-25 16:58:13 -0500

    added functionality to check for empty input fields

--- scripts/compareStrings.js
+++ scripts/compareStrings.js
@@ -5,8 +5,13 @@ var longerString  = function(){
 
   // check for whitespace
   if( string1.indexOf( " " ) >= 0 || string2.indexOf( " " ) >= 0 ){
-    console.log("Whitspace ROCKS!!!");
+    console.log("Whitespace ROCKS!!!");
   }
+
+  else if ( string1.length === 0 || string2.length === 0 ) {
+    alert ("You must fill out both input fields!");
+  }
+
   else {
     if( string1.length > string2.length ){
       console.log( string1 + " is the longest string" );
@@ -15,4 +20,11 @@ var longerString  = function(){
       console.log( string2 + " is the longest string" );
     }
   }
-}
+
+
+
+  // check for user input
+  // if ( string1.length === 0 || string2.length === 0 ) {
+  //   alert ("You must fill out both input fields!");
+  // }
+};

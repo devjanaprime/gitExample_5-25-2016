8a5a708f0a9bd8a872939518bccc89873db241f0
Maetz007 <maetz007@yahoo.com>
2016-05-25 16:43:47 -0500

    done!

--- scripts/compareStrings.js
+++ scripts/compareStrings.js
@@ -4,15 +4,20 @@ var longerString  = function(){
   var string2 = document.getElementById('word2').value;
 
   // check for whitespace
+if ( string1.length === 0 || string2.length === 0 ) {
+  console.log( "Inputs can't be left blank!" );
+}
+  else {
   if( string1.indexOf( " " ) >= 0 || string2.indexOf( " " ) >= 0 ){
     console.log("Whitspace ROCKS!!!");
-  }
-  else {
-    if( string1.length > string2.length ){
+    }
+    else {
+      if( string1.length > string2.length ){
       console.log( string1 + " is the longest string" );
-    }
-    else{
+      }
+      else{
       console.log( string2 + " is the longest string" );
-    }
+      }
+      }
   }
-}
+};

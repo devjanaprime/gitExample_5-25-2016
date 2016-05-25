f6e0ecee4ee5b19ceebc16fed9bbf254511ed921
Virginia Tyson <virginiatyson@Virginias-MacBook-Pro.local>
2016-05-25 16:57:51 -0500

    ginityson branch

---
+++ scripts/compareStrings.js
@@ -0,0 +1,20 @@
+var longerString  = function(){
+  // get user input
+  var string1 = document.getElementById('word1').value;
+  var string2 = document.getElementById('word2').value;
+  //check if inputs are empty
+  if( string1 === "" || string2 === "" ){
+    console.log("no empties");
+  }
+  else if( string1.indexOf( " " ) >= 0 || string2.indexOf( " " ) >= 0 ){
+    console.log("Whitspace ROCKS!!!");
+  }
+  else {
+    if( string1.length > string2.length ){
+      console.log( string1 + " is the longest string" );
+    }
+    else{
+      console.log( string2 + " is the longest string" );
+    }
+  }
+}

2256d8cd3f055a9cb0ae58c61d277529bb92113a
devjanaprime <dev@primeacademy.io>
2016-05-25 15:28:30 -0500

    basic user input

--- scripts/compareStrings.js
+++ scripts/compareStrings.js
@@ -1,4 +1,6 @@
-var longerString  = function( string1, string2 ){
+var longerString  = function(){
+  var string1 = document.getElementById('word1').value;
+  var string2 = document.getElementById('word2').value;
   console.log( 'in longerString: ' + string1 + " " + string2 );
   if( string1.length > string2.length ){
     console.log( string1 + " is the longest string" );
@@ -7,5 +9,3 @@ var longerString  = function( string1, string2 ){
     console.log( string2 + " is the longest string" );
   }
 }
-
-longerString( 'apples', 'oranges');

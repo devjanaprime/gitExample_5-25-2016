4e838962366f61ac32bf9761d3624d48795054ab
devjanaprime <dev@primeacademy.io>
2016-05-25 15:59:53 -0500

    whitespace check

--- scripts/compareStrings.js
+++ scripts/compareStrings.js
@@ -1,11 +1,18 @@
 var longerString  = function(){
+  // get user input
   var string1 = document.getElementById('word1').value;
   var string2 = document.getElementById('word2').value;
-  console.log( 'in longerString: ' + string1 + " " + string2 );
-  if( string1.length > string2.length ){
-    console.log( string1 + " is the longest string" );
+
+  // check for whitespace
+  if( string1.indexOf( " " ) >= 0 || string2.indexOf( " " ) >= 0 ){
+    console.log("Whitspace ROCKS!!!");
   }
-  else{
-    console.log( string2 + " is the longest string" );
+  else {
+    if( string1.length > string2.length ){
+      console.log( string1 + " is the longest string" );
+    }
+    else{
+      console.log( string2 + " is the longest string" );
+    }
   }
 }

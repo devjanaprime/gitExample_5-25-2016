8013c9bbcad3e28ef3fddc19d94c800375e1486f
HenryHall <HenryHall@Henrys-MacBook-Pro.local>
2016-05-25 16:38:10 -0500

    Henry's Changes
    quick code update

--- scripts/compareStrings.js
+++ scripts/compareStrings.js
@@ -4,15 +4,19 @@ var longerString  = function(){
   var string2 = document.getElementById('word2').value;
 
   // check for whitespace
-  if( string1.indexOf( " " ) >= 0 || string2.indexOf( " " ) >= 0 ){
-    console.log("Whitspace ROCKS!!!");
-  }
-  else {
+  if( string1 == "" || string2 == "" ){
+    document.getElementById("warning").innerHTML = "You must enter a string.";
+    document.getElementById("warning").style.background = "red";
+  } else if (string1.indexOf( " " ) >= 0 || string2.indexOf( " " ) >= 0 ) {
+    document.getElementById("warning").innerHTML = "You cannot use spaces.";
+    document.getElementById("warning").style.background = "red";
+  } else {
+    document.getElementById("warning").style.background = "green";
     if( string1.length > string2.length ){
-      console.log( string1 + " is the longest string" );
+      document.getElementById("warning").innerHTML = string1 + " is the longest string!";
     }
     else{
-      console.log( string2 + " is the longest string" );
+      document.getElementById("warning").innerHTML = string2 + " is the longest string!";
     }
   }
 }

4ba9a43843767ed46166c192fe85b0e8a05737c4
gitthew <Matthew@Matthews-MacBook-Pro.local>
2016-05-25 16:31:20 -0500

    Add check empty string functionality

--- scripts/compareStrings.js
+++ scripts/compareStrings.js
@@ -4,15 +4,15 @@ var longerString  = function(){
   var string2 = document.getElementById('word2').value;
 
   // check for whitespace
-  if( string1.indexOf( " " ) >= 0 || string2.indexOf( " " ) >= 0 ){
-    console.log("Whitspace ROCKS!!!");
-  }
-  else {
+  if (string1 === "" || string2 === "") {
+    console.log("Empty strings are a BUMMER!!!");
+  } else if ( string1.indexOf( " " ) >= 0 || string2.indexOf( " " ) >= 0 ){
+    console.log("NO whitespace allowed!!!");
+  } else {
     if( string1.length > string2.length ){
-      console.log( string1 + " is the longest string" );
-    }
-    else{
-      console.log( string2 + " is the longest string" );
+      console.log( string1 + " is the longest string!!!" );
+    } else{
+      console.log( string2 + " is the longest string!!!" );
     }
   }
-}
+};

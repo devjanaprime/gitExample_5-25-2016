6552afbb82fac88c6ae5b5bcd439da22c4d2c8a3
anna baker <annakatherinebaker@gmail.com>
2016-05-25 16:44:35 -0500

    Anna Baker commits

--- scripts/compareStrings.js
+++ scripts/compareStrings.js
@@ -2,9 +2,11 @@ var longerString  = function(){
   // get user input
   var string1 = document.getElementById('word1').value;
   var string2 = document.getElementById('word2').value;
-
-  // check for whitespace
-  if( string1.indexOf( " " ) >= 0 || string2.indexOf( " " ) >= 0 ){
+  if ( string1 === "" || string2 === "") {
+    console.log("no spaces please!");
+  }
+// check for whitespace
+  else if( string1.indexOf( " " ) >= 0 || string2.indexOf( " " ) >= 0 ){
     console.log("Whitspace ROCKS!!!");
   }
   else {
@@ -15,4 +17,4 @@ var longerString  = function(){
       console.log( string2 + " is the longest string" );
     }
   }
-}
+};

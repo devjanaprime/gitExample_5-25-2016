8b8ce016eaa28c6ac65b44d3675657fa0c2a329c
Maykid24 <patjster@gmail.com>
2016-05-25 17:01:01 -0500

    PatrickMay

--- scripts/compareStrings.js
+++ scripts/compareStrings.js
@@ -3,6 +3,17 @@ var longerString  = function(){
   var string1 = document.getElementById('word1').value;
   var string2 = document.getElementById('word2').value;
 
+  if( string1.indexOf(" ") >= 0 || string2.indexOf(" ") >= 0){
+    console.log("Come on... No spaces...");
+  }
+  else {
+    if( string1.length > string2.lenght){
+    console.log(string1 + " Is shorter... Tricked you :P");
+    }
+    else {
+      console.log(string2 + " Is longer! Opposite day :P");
+    }
+  }
   // check for whitespace
   if( string1.indexOf( " " ) >= 0 || string2.indexOf( " " ) >= 0 ){
     console.log("Whitspace ROCKS!!!");
@@ -15,4 +26,4 @@ var longerString  = function(){
       console.log( string2 + " is the longest string" );
     }
   }
-}
+};
